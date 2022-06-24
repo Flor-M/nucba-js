0e2a43959670daca16eb6e20858151fc39c5ef84
Flor-M <florenciamotta1@gmail.com>
2022-06-24 19:41:23 -0300

    Mejoras varias a E5

--- E5/e5.js
+++ E5/e5.js
@@ -52,19 +52,75 @@ En este ultimo desafío general vamos a utilizar el mismo array "Pizzas🍕":
 👉 Crear una barra de búsqueda (input), la cual tenga la función de mostrarnos solo las pizzas 
 cuyos nombres coincidan con la búsqueda realizada.  */
 
+const allPizzas = document.querySelector('#all-pizzas');
 const infoPizza = document.querySelector('#info-pizza');
 const formPizza = document.querySelector('#form-pizza');
 
 const imprimirStock = () => {
-
+    
     const pizzasExistentes = localStorage.getItem('pizzas');
     const pizzasStock = JSON.parse(pizzasExistentes);
+    /*
+    <div class="card-tiny">
+            <div class="tiny-text-container">
+                <div class="tiny-primer-info">
+                    <p id="tiny-nombre" class="tiny-name">Fugazzeta</p>
+                    <span id="tiny-precio" class="tiny-price">$650</span>
+                </div>
+                <p id="tiny-ingredientes" class="tiny-ingredients">muzarella, tomate, oregano, aceitunas</p>
+            </div>
+            <div id="tiny-img-container" class="tiny-img-contenedor">
+                <img src="https://i0.wp.com/safariresto.com.ar/wp-content/uploads/2020/10/pizza-fugazzeta.jpg?fit=1000%2C1000&ssl=1">
+            </div>
+        </div> 
+    */
+    pizzasStock.forEach(pizza => {
+        
+        const tinyCard = document.createElement('div');
+        tinyCard.classList.add("card-tiny");
 
-    const pizzasDisponible = document.querySelector('#nombres-disp');
+        const tinyTextContainer = document.createElement('div');
 
-    const pizzasDisponibleText = document.createTextNode(`${pizzasStock.map(Pizza => Pizza.nombre).join(' - ')}`);
-    pizzasDisponible.appendChild(pizzasDisponibleText);
+        const tinyPrimerInfo = document.createElement('div');
+        tinyPrimerInfo.classList.add("tiny-primer-info");
 
+        const tinyNombre = document.createElement('p');
+        const textNomb = document.createTextNode(pizza.nombre);
+        tinyNombre.appendChild(textNomb);
+        tinyNombre.classList.add("tiny-name");
+
+        const tinyPrecio = document.createElement('span');
+        const textPrec = document.createTextNode(`$${pizza.precio}`);
+        tinyPrecio.appendChild(textPrec);
+        tinyPrecio.classList.add("tiny-price");
+
+        const tinyIngred = document.createElement('p');
+        const textIngred = document.createTextNode(pizza.ingredientes.join(', '));
+        tinyIngred.appendChild(textIngred);
+        tinyIngred.classList.add("tiny-ingredients");
+
+        const tinyImgContainer = document.createElement('div');
+        tinyImgContainer.classList.add("tiny-img-contenedor");
+
+        const tinyImg = document.createElement('img');
+        tinyImg.setAttribute('src', pizza.imagen);
+        tinyImg.classList.add("tiny-img");
+
+        allPizzas.appendChild(tinyCard);
+
+        tinyCard.appendChild(tinyTextContainer);
+
+        tinyTextContainer.appendChild(tinyPrimerInfo);
+
+        tinyPrimerInfo.appendChild(tinyNombre);
+        tinyPrimerInfo.appendChild(tinyPrecio);
+
+        tinyTextContainer.appendChild(tinyIngred);
+
+        tinyCard.appendChild(tinyImgContainer);
+
+        tinyImgContainer.appendChild(tinyImg);
+    });
 };
 
 const limpiarData = () => {
